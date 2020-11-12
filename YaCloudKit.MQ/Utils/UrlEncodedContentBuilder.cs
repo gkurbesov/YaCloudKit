d58d6fe62d1cb534d5dca6d1ac52b428e45f5f85
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-11-12 20:35:35 +0300

    Добавил класс для создания строки запроса

---
+++ YaCloudKit.MQ/Utils/UrlEncodedContentBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Utils
+{
+    internal static class UrlEncodedContentBuilder
+    {
+        public static byte[] GetContentByteArray(IEnumerable<KeyValuePair<string, string>> nameValueCollection)
+        {
+            if (nameValueCollection == null)
+                throw new ArgumentNullException(nameof(nameValueCollection));
+
+            StringBuilder builder = new StringBuilder();
+            foreach (KeyValuePair<string, string> pair in nameValueCollection)
+            {
+                if (builder.Length > 0)
+                    builder.Append('&');
+
+                builder.Append(Encode(pair.Key));
+                builder.Append('=');
+                builder.Append(Encode(pair.Value));
+            }
+            return Encoding.UTF8.GetBytes(builder.ToString());
+        }
+
+        public static string Encode(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+                return string.Empty;
+            return Uri.EscapeDataString(data).Replace("%20", "+");
+        }
+    }
+}
