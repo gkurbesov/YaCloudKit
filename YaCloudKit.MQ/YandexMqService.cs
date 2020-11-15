bc1c016d5c4e95686f8ad5f5b524d4c9be370b96
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-11-15 19:20:10 +0300

    Добавил базовый класас для работы с сервисом очередей

---
+++ YaCloudKit.MQ/YandexMqService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YaCloudKit.MQ.Utils;
+
+namespace YaCloudKit.MQ
+{
+    public abstract class YandexMqService
+    {
+        public YandexMqConfig Config { get; set; }
+
+        protected YandexMqService(YandexMqConfig config)
+        {
+            if (config == null)
+                throw new ArgumentNullException(nameof(config));
+
+            Config = config;
+        }
+
+        protected async Task<TResponse> InvokeAsync<TResponse>(InvokeOptions options, CancellationToken cancellationToken)
+        {
+            ThrowIfDisposed();
+
+            IRequestContext requestContext = options.RequestMarshaller.Marshall(options.OriginalRequest);
+            YandexMqHeaderBuilder.AddHeaders(requestContext, Config.EndPoint);
+
+            var signature = new YandexMqSigner(Config).Create(requestContext);
+            YandexMqHeaderBuilder.AddHeaderAuthorization(requestContext, signature);
+
+            return default;
+        }
+
+        protected void ThrowIfDisposed()
+        {
+
+        }
+    }
+}
