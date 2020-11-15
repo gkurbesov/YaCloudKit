8bbe513b0b42111137517b5b664c7555046c8308
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-11-15 19:20:25 +0300

    Добавил класс клиента

---
+++ YaCloudKit.MQ/YandexMqClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ
+{
+    public class YandexMqClient : YandexMqService
+    {
+        public YandexMqClient(string accessKeyID, string secretAccessKey) :
+            this(new YandexMqConfig(accessKeyID, secretAccessKey))
+        { }
+
+        public YandexMqClient(YandexMqConfig config) :
+            base(config)
+        { }
+    }
+}
