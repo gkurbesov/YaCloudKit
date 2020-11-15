668f9c3d2f6377ce228772c23c7187173dc091f0
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-11-15 19:17:09 +0300

    Добавил опции длявыполнения

---
+++ YaCloudKit.MQ/InvokeOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YaCloudKit.MQ.Marshallers;
+using YaCloudKit.MQ.Model;
+using YaCloudKit.MQ.Model.Requests;
+
+namespace YaCloudKit.MQ
+{
+    public class InvokeOptions
+    {
+        public BaseRequest OriginalRequest { get; set; }
+        public IMarshaller<BaseRequest> RequestMarshaller { get; set; }
+    }
+}
