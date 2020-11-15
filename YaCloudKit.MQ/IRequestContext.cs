9a2e65d209299ec932ca62574b7c41f64abfa760
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-11-15 19:16:47 +0300

    Добавил контекст для запроса

---
+++ YaCloudKit.MQ/IRequestContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ
+{
+    public interface IRequestContext
+    {
+        IDictionary<string, string> RequestParameters { get; set; }
+        IDictionary<string, string> Headers { get; set; }
+        DateTime RequestDateTime { get; set; }
+
+        IRequestContext AddParametr(string key, string value);
+        IRequestContext AddHeader(string key, string value);
+        byte[] GetContent();
+
+    }
+}
