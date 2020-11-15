e0cdadefdcc747796ca04a9529783d603b1f87e0
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-11-15 20:43:40 +0300

    Добавил интерфейс обратной маршализации для получения результатов от сервера

---
+++ YaCloudKit.MQ/Marshallers/IUnmarshaller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaCloudKit.MQ.Marshallers
+{
+    public interface IUnmarshallerr
+    {
+        Task<T> UnmarshallAsync<T>(HttpResponseMessage response);
+    }
+}
