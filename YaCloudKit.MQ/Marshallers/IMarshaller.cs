fb78902477354198b409e08227ffa6a325011106
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-11-15 19:18:24 +0300

    Добавил интерфейс маршаллера запросов

---
+++ YaCloudKit.MQ/Marshallers/IMarshaller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Marshallers
+{
+    public interface IMarshaller<T>
+    {
+        IRequestContext Marshall(T input);
+    }
+}
