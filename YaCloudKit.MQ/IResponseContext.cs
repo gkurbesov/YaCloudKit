eb73b3858855a08a588800d410982ecc6be806d3
Gennady <ltrg67@gmail.com>
2020-12-09 17:27:15 +0300

    Добавление контекста ответа

---
+++ YaCloudKit.MQ/IResponseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace YaCloudKit.MQ
+{
+    public interface IResponseContext
+    {
+        HttpStatusCode StatusCode { get; }
+        HttpResponseHeaders Headers { get; }
+        Stream ContentStream { get; }
+    }
+}
---
+++ YaCloudKit.MQ/ResponseContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace YaCloudKit.MQ
+{
+    public class ResponseContext : IResponseContext
+    {
+        public HttpStatusCode StatusCode { get; private set; }
+
+        public HttpResponseHeaders Headers { get; private set; }
+
+        public Stream ContentStream { get; private set; }
+
+        public ResponseContext(HttpStatusCode httpStatusCode, HttpResponseHeaders headers, Stream content)
+        {
+            StatusCode = httpStatusCode;
+            Headers = headers;
+            ContentStream = content;
+        }
+    }
+}
