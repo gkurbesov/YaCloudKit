3227a37ad79752553eabcc760224a77c9b9137a5
Gennady <ltrg67@gmail.com>
2020-12-11 08:21:39 +0300

    Вспомогательный класс для генерации атрибутов запроса

---
+++ YaCloudKit.MQ/Utils/RequestAttributesBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Utils
+{
+    public static class RequestAttributesBuilder
+    {
+        public static void NamedAttributes(IRequestContext context, Dictionary<string, string> values)
+        {
+            var number = 1;
+            foreach(var item in values)
+            {
+                context.AddParametr($"Attribute.{number}.Name", item.Key);
+                context.AddParametr($"Attribute.{number}.Value", item.Value);
+                number++;
+            }
+        }
+    }
+}
