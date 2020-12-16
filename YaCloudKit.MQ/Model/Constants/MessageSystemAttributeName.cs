f2012a8e809b440d5a5172b107b0c3581091e7ac
Gennady <ltrg67@gmail.com>
2020-12-16 18:36:14 +0300

    Добавил константы с именами атрибутов сообщений

---
+++ YaCloudKit.MQ/Model/Constants/MessageSystemAttributeName.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Model.Constants
+{
+    /// <summary>
+    /// Класс содержащий константы имен атрибутов для сообщений
+    /// </summary>
+    public class MessageSystemAttributeName
+    {
+        /// <summary>
+        /// Используется для запроса всех атрибутов сообщения.
+        /// </summary>
+        public const string All = "All";
+        /// <summary>
+        /// Время получения сообщения из очереди.
+        /// </summary>
+        public const string ApproximateFirstReceiveTimestamp = "ApproximateFirstReceiveTimestamp";
+        /// <summary>
+        /// Число получений сообщения из очереди без его удаления.
+        /// </summary>
+        public const string ApproximateReceiveCount = "ApproximateReceiveCount";
+        /// <summary>
+        /// Идентификатор отправителя — субъекта IAM.
+        /// </summary>
+        public const string SenderId = "SenderId";
+        /// <summary>
+        /// Время отправки сообщения в очередь.
+        /// </summary>
+        public const string SentTimestamp = "SentTimestamp";
+        /// <summary>
+        /// Идентификатор токена для дедупликации сообщений, используется в очередях FIFO.
+        /// Каждое сообщение должно иметь уникальный MessageDeduplicationId. 
+        /// Если MessageDeduplicationId не указан, отправка сообщения в очередь не будет выполнена. 
+        /// Максимальная длина — 128 символов. Разрешено использование цифр, больших и маленьких латинских букв и знаков пунктуации. 
+        /// Подробнее см. Дедупликация.
+        /// </summary>
+        public const string MessageDeduplicationId = "MessageDeduplicationId";
+        /// <summary>
+        /// Идентификатор группы сообщений, используется в очередях FIFO. Подробнее см. Дедупликация.
+        /// </summary>
+        public const string MessageGroupId = "MessageGroupId";
+        /// <summary>
+        /// Номер сообщения, используется в очередях FIFO в рамках группы сообщений с одинаковым MessageGroupId.
+        /// </summary>
+        public const string SequenceNumber = "SequenceNumber";
+    }
+}
