a5b768fadfabfa8a7dc466b06799e67ec471164b
Gennady <ltrg67@gmail.com>
2020-12-21 00:58:39 +0300

    Добавил класс ответа

---
+++ YaCloudKit.TTS/YandexTtsResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace YaCloudKit.TTS
+{
+    public class YandexTtsResponse
+    {
+        /// <summary>
+        /// Статускод ответа
+        /// </summary>
+        public HttpStatusCode StatusCode { get; set; }
+        /// <summary>
+        /// GUID запроса, присваивается при включении логирования со стороны Yandex SpeechKit
+        /// </summary>
+        public string RequestId { get; set; }
+        /// <summary>
+        /// Результат синтеза речи
+        /// </summary>
+        public Stream Content { get; set; }
+    }
+}
