7c9efedaee2e520cc8c14d4187e11fd0f8d2d517
Gennady <ltrg67@gmail.com>
2020-12-21 01:01:09 +0300

    Добавил интерфейс сервиса

--- YaCloudKit.TTS/IYandexTts.cs
+++ YaCloudKit.TTS/IYandexTts.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using YaCloudKit.TTS.Model;
 
 namespace YaCloudKit.TTS
 {
     public interface IYandexTts
     {
-
+        Task<YandexTtsResponse> TextToSpeech(string text, VoiceParameters voice, FormatParameters format);
+        Task<YandexTtsResponse> MarkupToSpeech(string text, VoiceParameters voice, FormatParameters format);
     }
 }
