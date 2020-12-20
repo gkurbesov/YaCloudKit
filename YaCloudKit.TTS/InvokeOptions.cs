5b82d8f2b48f6f8c721cde8a19edc0fa5516d71f
Gennady <ltrg67@gmail.com>
2020-12-21 00:25:34 +0300

    Класс с параметрами запроса

---
+++ YaCloudKit.TTS/InvokeOptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YaCloudKit.TTS.Model;
+
+namespace YaCloudKit.TTS
+{
+    /// <summary>
+    /// Опции выполнения запроса
+    /// </summary>
+    public class InvokeOptions
+    {
+        /// <summary>
+        /// Текст для синтеза речи
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// Флаг использования SSML
+        /// </summary>
+        public bool SSML { get; set; }
+        /// <summary>
+        /// Параметры голоса для синтеза
+        /// </summary>
+        public VoiceParameters Voice { get; set; }
+        /// <summary>
+        /// ПЕараметры выходных данных ауди-файла
+        /// </summary>
+        public FormatParameters AudioFormat { get; set; }
+    }
+}
