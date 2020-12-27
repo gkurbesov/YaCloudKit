d69c4d0082ed68c88967bb861f3ac107e4459aa3
Gennady <ltrg67@gmail.com>
2020-12-27 11:04:36 +0300

    Добавил коментарии

--- YaCloudKit.TTS/IYandexTts.cs
+++ YaCloudKit.TTS/IYandexTts.cs
@@ -9,7 +9,23 @@ namespace YaCloudKit.TTS
 {
     public interface IYandexTts
     {
+        /// <summary>
+        /// Синтезировать речь из обычного текста
+        /// </summary>
+        /// <param name="text">текст для генерации речи</param>
+        /// <param name="voice">параметры голоса</param>
+        /// <param name="format">параметры выходного формата</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<YandexTtsResponse> TextToSpeechAsync(string text, VoiceParameters voice, FormatParameters format, CancellationToken cancellationToken = default);
+        /// <summary>
+        /// Синтезировать речь из текста в формате SSML
+        /// </summary>
+        /// <param name="text">текст для генерации речи</param>
+        /// <param name="voice">параметры голоса</param>
+        /// <param name="format">параметры выходного формата</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<YandexTtsResponse> MarkupToSpeechAsync(string text, VoiceParameters voice, FormatParameters format, CancellationToken cancellationToken = default);
     }
 }
