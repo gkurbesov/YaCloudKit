f33d6e7f9be7d65a1e59a829ae8cbbdcd5d05e25
Gennady <ltrg67@gmail.com>
2021-01-24 19:33:04 +0300

    Добавил результат запроса на создание нового IAM-токена

---
+++ YaCloudKit.IAM/IamTokenCreateResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.IAM
+{
+    /// <summary>
+    /// Результат запроса на создание нового IAM-токена
+    /// </summary>
+    public class IamTokenCreateResult
+    {
+        /// <summary>
+        /// IAM-токен
+        /// </summary>
+        public string iamToken { get; set; }
+        /// <summary>
+        /// Время окончания действия IAM-токена. Строка в формате RFC3339.
+        /// </summary>
+        public string expiresAt { get; set; }
+    }
+}
