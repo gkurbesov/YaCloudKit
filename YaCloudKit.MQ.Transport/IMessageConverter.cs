b179410897392dd596ce5aa385888deeb8f26c12
Gennady <ltrg67@gmail.com>
2021-03-17 10:04:12 +0300

    Добавил интерфейс конвертора сообщений

---
+++ YaCloudKit.MQ.Transport/IMessageConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public interface IMessageConverter
+    {
+        T Deserialize<T>(string messageBody);
+        object Deserialize(string messageBody, Type type);
+        string Serialize(object value);
+    }
+}
