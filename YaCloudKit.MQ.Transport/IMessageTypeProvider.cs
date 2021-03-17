291adb712fe564b827e91552b74c7aa2d35fab81
Gennady <ltrg67@gmail.com>
2021-03-17 09:57:43 +0300

    Добавил интерфейс провайдера типов сообщений

---
+++ YaCloudKit.MQ.Transport/IMessageTypeProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public interface IMessageTypeProvider
+    {
+        void Registration(string tag, Type type);
+        Type GetMessageType(string tag);
+    }
+}
