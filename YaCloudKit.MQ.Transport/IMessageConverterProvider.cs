b310910852ac9d731554ac7b790599c6cd16dc58
Gennady <ltrg67@gmail.com>
2021-03-17 10:06:21 +0300

    добавил интерфейс провайдера конверторов

---
+++ YaCloudKit.MQ.Transport/IMessageConverterProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public interface IMessageConverterProvider
+    {
+        void Registration(string tag, IMessageConverter converter);
+        IMessageConverter GetConverter(string tag);
+    }
+}
