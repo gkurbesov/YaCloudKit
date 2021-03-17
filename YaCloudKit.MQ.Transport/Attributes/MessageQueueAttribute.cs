bc2343ae231b198b37719d5b4221c5b9747e48af
Gennady <ltrg67@gmail.com>
2021-03-17 09:44:23 +0300

    Добавил атрибут для сериализации пользовательских классов сообщений

---
+++ YaCloudKit.MQ.Transport/Attributes/MessageQueueAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Transport.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class MessageQueueAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public MessageQueueAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
