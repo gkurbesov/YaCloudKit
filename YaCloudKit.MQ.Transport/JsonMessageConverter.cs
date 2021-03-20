8314ffcc15012836e8de5a997b7625793e73d281
Gennady <ltrg67@gmail.com>
2021-03-20 23:46:34 +0300

    Добавил Json кеонвертер

---
+++ YaCloudKit.MQ.Transport/JsonMessageConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public class JsonMessageConverter : IMessageConverter
+    {
+        public T Deserialize<T>(string messageBody)
+        {
+            return JsonConvert.DeserializeObject<T>(messageBody);
+        }
+
+        public object Deserialize(string messageBody, Type type)
+        {
+            return JsonConvert.DeserializeObject(messageBody, type);
+        }
+
+        public string Serialize(object value)
+        {
+            return Serialize(value, Formatting.None);
+        }
+
+        public string Serialize(object value, Formatting formatting, JsonSerializerSettings settings = null)
+        {
+            return JsonConvert.SerializeObject(value, formatting, settings);
+        }
+    }
+}
