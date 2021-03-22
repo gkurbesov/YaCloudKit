355b13698f4ec7b2ab6818cde5a6b6c440b66317
Gennady <ltrg67@gmail.com>
2021-03-22 19:51:08 +0300

    Добавил XML конвертер

---
+++ YaCloudKit.MQ.Transport/Converters/XmlMessageConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace YaCloudKit.MQ.Transport.Converters
+{
+    public class XmlMessageConverter : IMessageConverter
+    {
+        public T Deserialize<T>(string messageBody) where T : class
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(messageBody)))
+            {
+                var result = serializer.Deserialize(ms) as T;
+                return result;
+            }
+        }
+
+        public object Deserialize(string messageBody, Type type)
+        {
+            XmlSerializer serializer = new XmlSerializer(type);
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(messageBody)))
+            {
+                var result = serializer.Deserialize(ms);
+                return result;
+            }
+        }
+
+        public string Serialize(object value)
+        {
+            XmlSerializer serializer = new XmlSerializer(value.GetType());
+            using (MemoryStream ms = new MemoryStream())
+            {
+                serializer.Serialize(ms, value);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+    }
+}
