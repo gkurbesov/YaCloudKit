f86192e318965f10f4e2e548b301f79a8a3bc1f5
Gennady <ltrg67@gmail.com>
2021-03-22 19:59:15 +0300

    Добавил провайдер типов сообщений

---
+++ YaCloudKit.MQ.Transport/MessageTypeProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public class MessageTypeProvider : IMessageTypeProvider
+    {
+        private readonly Dictionary<string, Type> types = new Dictionary<string, Type>();
+
+        public MessageTypeProvider() { }
+
+        public Type GetMessageType(string tag)
+        {
+            return types.ContainsKey(tag) ? types[tag] : null;
+        }
+
+        public void Register<T>(string tag)
+        {
+            Register(tag, typeof(T));
+        }
+
+        public void Register(string tag, Type type)
+        {
+            if (types.ContainsKey(tag))
+                types[tag] = type;
+            else
+                types.Add(tag, type);
+        }
+    }
+}
