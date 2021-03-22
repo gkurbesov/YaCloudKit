79eaf07a713bbb23485c23f3ab9eedb2c8a69324
Gennady <ltrg67@gmail.com>
2021-03-22 19:56:11 +0300

    Добавил провайдер конверторов

---
+++ YaCloudKit.MQ.Transport/MessageConverterProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public class MessageConverterProvider : IMessageConverterProvider
+    {
+        private readonly Dictionary<string, IMessageConverter> converters = new Dictionary<string, IMessageConverter>();
+
+        public MessageConverterProvider() { }
+
+        public IMessageConverter GetConverter(string tag)
+        {
+            return converters.ContainsKey(tag) ? converters[tag] : null;
+        }
+
+        public void Register(string tag, IMessageConverter converter)
+        {
+            if (converters.ContainsKey(tag))
+                converters[tag] = converter;
+            else
+                converters.Add(tag, converter);
+        }
+    }
+}
