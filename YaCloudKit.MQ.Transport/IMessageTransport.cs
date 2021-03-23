1c884857bc06047b461325a86013800ab4df51e6
Gennady <ltrg67@gmail.com>
2021-03-23 18:34:25 +0300

    Добавил интерфейс транспорта

---
+++ YaCloudKit.MQ.Transport/IMessageTransport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YaCloudKit.MQ.Model.Requests;
+using YaCloudKit.MQ.Model.Responses;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public interface IMessageTransport
+    {
+        Task<IEnumerable<object>> ReceiveMessageAsync(ReceiveMessageRequest message, CancellationToken cancellationToken = default);
+        Task<SendMessageResponse> SendMessageAsync<T>(T message, CancellationToken cancellationToken = default);
+    }
+}
