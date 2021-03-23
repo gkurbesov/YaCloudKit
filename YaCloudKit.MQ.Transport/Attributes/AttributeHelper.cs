1bfc2d40d176d3e635969bbf0a33701c160d6ea1
Gennady <ltrg67@gmail.com>
2021-03-23 15:11:47 +0300

    Метод для получения названия сообщения

---
+++ YaCloudKit.MQ.Transport/Attributes/AttributeHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Transport.Attributes
+{
+    public class AttributeHelper
+    {
+        public static string GetMessageName(object value)
+        {
+            var attributes = value.GetType().GetCustomAttributes(true);
+            foreach(var attr in attributes)
+            {
+                if(attr is MessageQueueAttribute messageAttribute)
+                {
+                    return messageAttribute.Name;
+                }
+            }
+            return value.GetType().Name;
+        }
+    }
+}
