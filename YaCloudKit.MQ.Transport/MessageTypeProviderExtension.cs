826cda8a10f4878c350721afb177e2ab877b92bd
Gennady <ltrg67@gmail.com>
2021-03-25 16:51:18 +0300

    добавил методы расширения для MessageTypeProvider

---
+++ YaCloudKit.MQ.Transport/MessageTypeProviderExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YaCloudKit.MQ.Transport.Attributes;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public static class MessageTypeProviderExtension
+    {
+        public static IMessageTypeProvider Register(this IMessageTypeProvider provider, Type type)
+        {
+            var tag = AttributeHelper.GetPropertyName<MessageQueueAttribute>(type);
+            return provider.Register(tag, type);            
+        }
+
+        public static IMessageTypeProvider Register<T>(this IMessageTypeProvider provider)
+        {
+            var type = typeof(T);
+            var tag = AttributeHelper.GetPropertyName<MessageQueueAttribute>(type);
+            return provider.Register(tag, type);
+        }
+
+    }
+}
