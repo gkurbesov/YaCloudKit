e7873a3272a0e0481d68477b75b5b053c745aa2d
Gennady <ltrg67@gmail.com>
2021-03-25 16:53:28 +0300

    Добавил класс с конфигурацией транспорта

---
+++ YaCloudKit.MQ.Transport/YandexMqTrasport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YaCloudKit.MQ.Transport.Converters;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public static class YandexMqTrasport
+    {
+        public static readonly IMessageConverterProvider ConverterProvider = new MessageConverterProvider();
+        public static readonly IMessageTypeProvider TypeProvider = new MessageTypeProvider();
+
+        public static void Initialize(Action<IMessageTypeProvider> configure)
+        {
+            ConverterProvider.Register(JsonMessageConverter.TAG, new JsonMessageConverter());
+            ConverterProvider.Register(XmlMessageConverter.TAG, new XmlMessageConverter());
+
+            configure(TypeProvider);
+        }
+
+        public static void Initialize(Action<IMessageConverterProvider, IMessageTypeProvider> configure)
+        {
+            configure(ConverterProvider, TypeProvider);
+        }
+    }
+}
