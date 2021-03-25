2b19f36a58ec722221c7058f251cb546471cb7ed
Gennady <ltrg67@gmail.com>
2021-03-25 16:20:30 +0300

    добавил атрибут для определения конвертера

---
+++ YaCloudKit.MQ.Transport/Attributes/MessageConverterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Transport.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class MessageConverterAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public MessageConverterAttribute(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name));
+            Name = name;
+        }
+    }
+}
