9f4e2364814f2ed45532c68ed274a636e99a17b2
Gennady <ltrg67@gmail.com>
2021-03-26 09:58:22 +0300

    Добавил исключение

---
+++ YaCloudKit.MQ.Transport/YandexMqTrasportException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public class YandexMqTrasportException : Exception
+    {
+        public YandexMqTrasportException() { }
+        public YandexMqTrasportException(string message)
+            : base(message) { }
+
+        public YandexMqTrasportException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
