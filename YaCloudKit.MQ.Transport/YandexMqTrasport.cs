1e0aeed07db478d022a6d32028ad422a195367ab
Gennady <ltrg67@gmail.com>
2021-06-09 23:35:50 +0300

    Изменил инициализацию транспорта

--- YaCloudKit.MQ.Transport/YandexMqTrasport.cs
+++ YaCloudKit.MQ.Transport/YandexMqTrasport.cs
@@ -13,16 +13,24 @@ namespace YaCloudKit.MQ.Transport
 
         public static readonly IMessageConverterProvider ConverterProvider = new MessageConverterProvider();
         public static readonly IMessageTypeProvider TypeProvider = new MessageTypeProvider();
+
+        internal static volatile bool isInitialized = false;
+
         /// <summary>
         /// Инициализация расширения функционала клиента очереди сообщений
         /// </summary>
         /// <param name="configure">Действие для регистрации используемых типов сообщений</param>
         public static void Initialize(Action<IMessageTypeProvider> configure)
         {
+            if (isInitialized)
+                throw new OperationCanceledException("Yandex Message Queue Trasport is initialized");
+
             ConverterProvider.Register(JsonMessageConverter.TAG, new JsonMessageConverter());
             ConverterProvider.Register(XmlMessageConverter.TAG, new XmlMessageConverter());
 
             configure(TypeProvider);
+
+            isInitialized = true;
         }
         /// <summary>
         /// Инициализация расширения функционала клиента очереди сообщений
@@ -30,12 +38,17 @@ namespace YaCloudKit.MQ.Transport
         /// <param name="configure">Действие для регистрации используемых конвертеров и типов сообщений</param>
         public static void Initialize(Action<IMessageConverterProvider, IMessageTypeProvider> configure)
         {
+            if (isInitialized)
+                throw new OperationCanceledException("Yandex Message Queue Trasport is initialized");
+
             configure(ConverterProvider, TypeProvider);
+
+            isInitialized = true;
         }
 
         internal static void ThrowIfNotInitialized()
         {
-            if (ConverterProvider.Values.Count() == 0)
+            if (!isInitialized)
                 throw new OperationCanceledException("Yandex Message Queue Trasport not initialized");
         }
     }
