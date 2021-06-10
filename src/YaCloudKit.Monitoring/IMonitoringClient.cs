78cfbc6a5d06625236c2a06b3ed2d71365a269a1
Gennady <ltrg67@gmail.com>
2021-06-11 00:43:24 +0300

    Добавил интерфейс для работы с метриками

---
+++ src/YaCloudKit.Monitoring/IMonitoringClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YaCloudKit.Monitoring.Models.Requests;
+using YaCloudKit.Monitoring.Models.Responses;
+
+namespace YaCloudKit.Monitoring
+{
+    public interface IMonitoringClient
+    {
+        Task<WriteResponse> Write(WriteRequest request, CancellationToken cancellationToken = default);
+    }
+}
