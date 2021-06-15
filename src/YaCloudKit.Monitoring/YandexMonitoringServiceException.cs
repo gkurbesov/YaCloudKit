b92556782bad9ff93ce025ea740a5c494ebea569
Gennady <ltrg67@gmail.com>
2021-06-16 01:16:40 +0300

    добавил класс исключения

---
+++ src/YaCloudKit.Monitoring/YandexMonitoringServiceException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace YaCloudKit.Monitoring
+{
+    public class YandexMonitoringServiceException : Exception
+    {
+        /// <summary>
+        /// Статус-код результата выполнения HTTP запроса
+        /// </summary>
+        public HttpStatusCode StatusCode { get; set; }
+
+        public YandexMonitoringServiceException() { }
+        public YandexMonitoringServiceException(string message)
+            : base(message) { }
+
+        public YandexMonitoringServiceException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        public YandexMonitoringServiceException(string message, Exception innerException, HttpStatusCode statusCode)
+            : base(message, innerException)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
