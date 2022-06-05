bc5b9ae31ffdd9c9f75ee93487d990de9f2b52e3
Gennady <ltrg67@gmail.com>
2022-06-06 00:28:17 +0300

    Коммит для теста

---
+++ src/MessageQueue/YaCloudKit.MQ.Tests/Utils/YandexMqHeaderBuilderTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace YaCloudKit.MQ.Tests.Utils
+{
+    public class YandexMqHeaderBuilderTests
+    {
+        [Fact]
+        public void AddMainHeaders_Test()
+        {
+            var context = new RequestContext();
+            context.AddParametr("key", "value");
+
+            var contentLength = context.GetContent().Length;
+
+
+            Assert.True(contentLength > 0);
+        }
+    }
+}
