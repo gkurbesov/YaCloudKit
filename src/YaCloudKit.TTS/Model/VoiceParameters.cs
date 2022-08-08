610949c2c26527b65a2d786a9543f3cc2fc9bf79
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-08-08 11:52:15 +0300

    Удалил константы

--- src/YaCloudKit.TTS/Model/VoiceParameters.cs
+++ src/YaCloudKit.TTS/Model/VoiceParameters.cs
@@ -1,26 +1,12 @@
 ﻿using System;
 
-namespace YaCloudKit.TTS.Model
+namespace YaCloudKit.TTS
 {
     /// <summary>
     /// Параметры голоса для генерации
     /// </summary>
     public class VoiceParameters
     {
-        /// <summary>
-        /// Эмоция голоса - доброжелательный
-        /// </summary>
-        public const string EMOTION_GOOD = "good";
-        /// <summary>
-        /// Эмоция голоса - злой
-        /// </summary>
-        public const string EMOTION_EVIL = "evil";
-        /// <summary>
-        /// Эмоция голоса - нейтральный
-        /// </summary>
-        public const string EMOTION_NEUTRAL = "neutral";
-
-
         /// <summary>
         /// Женский русский голос Oksana
         /// </summary>
