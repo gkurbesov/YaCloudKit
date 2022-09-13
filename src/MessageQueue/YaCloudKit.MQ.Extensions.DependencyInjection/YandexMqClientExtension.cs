8487c50a6f9b618474dfca9bd6e608e650bc86bb
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-09-13 20:43:01 +0300

    Добавил методы расширения

---
+++ src/MessageQueue/YaCloudKit.MQ.Extensions.DependencyInjection/YandexMqClientExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace YaCloudKit.MQ.Extensions.DependencyInjection;
+
+public static class YandexMqClientExtension
+{
+    public static IServiceCollection AddYandexMqClient(this IServiceCollection services, string accessKeyId, string secretAccessKey)
+    {
+        services.AddHttpClient<IYandexMq, YandexMqClient>(client =>
+            new YandexMqClient(
+                config: new YandexMqConfig(accessKeyId, secretAccessKey),
+                httpClientFactory: () => client));
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddYandexMqClient(this IServiceCollection services, Func<IServiceProvider, YandexMqConfig> configFactory)
+    {
+        services.AddHttpClient<IYandexMq, YandexMqClient>((client, serviceProvider) =>
+            new YandexMqClient(
+                config: configFactory(serviceProvider),
+                httpClientFactory: () => client));
+        
+        return services;
+    }
+}
