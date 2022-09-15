20c293deac35b400e94f914273e8e47f1f5bb959
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-09-15 10:40:32 +0300

    Изменил класс исключения

---
+++ src/MessageQueue/YaCloudKit.MQ.Transport/MqTransportException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace YaCloudKit.MQ.Transport
+{
+    public class MqTransportException : Exception
+    {
+        public MqTransportException() { }
+        public MqTransportException(string message)
+            : base(message) { }
+
+        public MqTransportException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
