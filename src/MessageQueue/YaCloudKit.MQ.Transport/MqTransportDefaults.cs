9cde6265ae2430eaf66588ccd095175496ee0ce0
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-09-15 10:41:03 +0300

    Добавил деолтные строки

---
+++ src/MessageQueue/YaCloudKit.MQ.Transport/MqTransportDefaults.cs
@@ -0,0 +1,7 @@
+﻿namespace YaCloudKit.MQ.Transport;
+
+public static class MqTransportDefaults
+{
+    public const string AttributeMessageType = "ya-cloud-kit-mq-message";
+    public const string AttributeMessageConverter = "ya-cloud-kit-mq-converter";
+}
