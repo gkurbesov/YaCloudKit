c1df6d3fd60e2665f55c3d69e4dd9e05764cfed4
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-09-18 20:24:05 +0300

    Изменил интерфейс сервиса

--- src/MessageQueue/YaCloudKit.MQ.Transport/IMqTransportService.cs
+++ src/MessageQueue/YaCloudKit.MQ.Transport/IMqTransportService.cs
@@ -1,12 +1,13 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using YaCloudKit.MQ.Model;
+using YaCloudKit.MQ.Model.Requests;
 
 namespace YaCloudKit.MQ.Transport;
 
 public interface IMqTransportService
 {
-    Task ReceiveAsync(Message message, CancellationToken cancellationToken);
+    Task HandleAsync(Message message, CancellationToken cancellationToken);
 
-    Task SendAsync<TMessage>(IYandexMq mq, string queueUrl, string converterName, TMessage message, CancellationToken cancellationToken);
+    Task<SendMessageRequest> TransformAsync<TMessage>(TMessage message, string converterName, CancellationToken cancellationToken);
 }
