8f198bc72978725113eeda680bb215e84b2bc32a
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-11-27 04:38:51 +0400

    Сгруппировал код по методам

--- samples/YaCloudKit.MQ.Transport.Examples/Program.cs
+++ samples/YaCloudKit.MQ.Transport.Examples/Program.cs
@@ -1,27 +1,6 @@
 ﻿using YaCloudKit.MQ.Transport.Examples;
 
-var producer = new ProducerService();
-
-var notification = new NotificationDto()
-{
-    Id = 7,
-    Title = "Hello World!",
-    Message = "This is test message notification"
-};
-
-Console.WriteLine("Send notification");
-await producer.Send(notification);
-
-await Task.Delay(TimeSpan.FromSeconds(2));
-Console.WriteLine("Send new user");
-var userRegistration = new UserRegistrationDto()
-{
-    UserId = 1,
-    Email = "example@gmail.com",
-    RegistrationDateTime = DateTime.Now
-};
-
-await producer.Send(userRegistration);
+await JobProducer();
 
 Console.WriteLine("Wait 5 seconds...");
 await Task.Delay(TimeSpan.FromSeconds(5));
@@ -29,11 +8,40 @@ await Task.Delay(TimeSpan.FromSeconds(5));
 Console.WriteLine("Press Enter");
 Console.ReadLine();
 
-var consumer = new ConsumerService();
-
-Console.WriteLine("Start receiving...");
-await consumer.Receive();
+await JobConsumer();
 
 Console.WriteLine("Completed!");
 
+async Task JobProducer()
+{
+    var producer = new ProducerService();
 
+    var notification = new NotificationDto()
+    {
+        Id = 7,
+        Title = "Hello World!",
+        Message = "This is test message notification"
+    };
+
+    Console.WriteLine("Send notification");
+    await producer.Send(notification);
+
+    await Task.Delay(TimeSpan.FromSeconds(2));
+    Console.WriteLine("Send new user");
+    var userRegistration = new UserRegistrationDto()
+    {
+        UserId = 1,
+        Email = "example@gmail.com",
+        RegistrationDateTime = DateTime.Now
+    };
+
+    await producer.Send(userRegistration);
+}
+
+async Task JobConsumer()
+{
+    var consumer = new ConsumerService();
+
+    Console.WriteLine("Start receiving...");
+    await consumer.Receive();
+}
