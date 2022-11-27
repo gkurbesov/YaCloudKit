72e0bcc4d95f2933a5a6542633a8279189edb07c
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-11-27 05:32:18 +0400

    Добавил пакет с методами расширения дл DI

---
+++ src/TextToSpeech/YaCloudKit.TTS.DependencyInjection/YandexTtsClientExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace YaCloudKit.TTS.DependencyInjection
+{
+    public static class YandexTtsClientExtensions
+    {
+        public static IServiceCollection AddYandexTtsClient(this IServiceCollection services, string apiKey)
+        {
+            services.AddHttpClient<IYandexTts, YandexTtsClient>(client =>
+                new YandexTtsClient(
+                    config: new YandexTtsConfig(apiKey),
+                    httpClientFactory: () => client));
+
+            return services;
+        }
+
+        public static IServiceCollection AddYandexTtsClient(this IServiceCollection services, string iam, string folderId)
+        {
+            services.AddHttpClient<IYandexTts, YandexTtsClient>(client =>
+                new YandexTtsClient(
+                    config: new YandexTtsConfig(iam, folderId),
+                    httpClientFactory: () => client));
+
+            return services;
+        }
+
+        public static IServiceCollection AddYandexTtsClient(this IServiceCollection services,
+            Func<IServiceProvider, YandexTtsConfig> configFactory)
+        {
+            services.AddHttpClient<IYandexTts, YandexTtsClient>((client, serviceProvider) =>
+                new YandexTtsClient(
+                    config: configFactory(serviceProvider),
+                    httpClientFactory: () => client));
+
+            return services;
+        }
+    }
+}
