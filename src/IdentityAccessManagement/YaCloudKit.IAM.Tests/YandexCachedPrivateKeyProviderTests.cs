8e4e34ab26405d3e4f9741abb48a65916d1057f1
Gennady Kurbesov <39064929+gkurbesov@users.noreply.github.com>
2024-11-24 00:14:19 +0400

    Add tests for IAM project (#20)

---
+++ src/IdentityAccessManagement/YaCloudKit.IAM.Tests/YandexCachedPrivateKeyProviderTests.cs
@@ -0,0 +1,51 @@
+﻿using YaCloudKit.IAM.Rsa;
+
+namespace YaCloudKit.IAM.Tests;
+
+[TestClass]
+public class YandexCachedPrivateKeyProviderTests
+{
+    [TestMethod]
+    public async Task GetPrivateKeyAsync_WhenPrivateKeyIsNotCached_ShouldCallFuncAgain()
+    {
+        // Arrange
+        var count = 0;
+
+        var privateKeyFunc = new Func<CancellationToken, Task<char[]>>(async _ =>
+        {
+            count++;
+            return "privateKey".ToCharArray();
+        });
+
+        var privateKeyProvider = new YandexFuncPrivateKeyProvider(privateKeyFunc);
+
+        // Act
+        _ = privateKeyProvider.GetPrivateKeyAsync(CancellationToken.None);
+        _ = privateKeyProvider.GetPrivateKeyAsync(CancellationToken.None);
+        
+        // Assert
+        Assert.AreEqual(2, count);
+    }
+    
+    [TestMethod]
+    public async Task GetPrivateKeyAsync_WhenPrivateKeyIsCached_ShouldNotCallFuncAgain()
+    {
+        // Arrange
+        var count = 0;
+
+        var privateKeyFunc = new Func<CancellationToken, Task<char[]>>(async _ =>
+        {
+            count++;
+            return "privateKey".ToCharArray();
+        });
+
+        var privateKeyProvider = new YandexFuncPrivateKeyProvider(privateKeyFunc, true);
+
+        // Act
+        _ = privateKeyProvider.GetPrivateKeyAsync(CancellationToken.None);
+        _ = privateKeyProvider.GetPrivateKeyAsync(CancellationToken.None);
+        
+        // Assert
+        Assert.AreEqual(1, count);
+    }
+}
