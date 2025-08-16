7092eb63db608fff7be0e0772605254c4fd44f20
Gennady Kurbesov <ltrg67@gmail.com>
2025-08-17 02:00:50 +0400

    рефакторинг генератора jwt

--- src/IdentityAccessManagement/YaCloudKit.IAM/Jwt/YandexJsonWebTokenGenerator.cs
+++ src/IdentityAccessManagement/YaCloudKit.IAM/Jwt/YandexJsonWebTokenGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using YaCloudKit.IAM.Rsa;
@@ -7,30 +8,45 @@ using YaCloudKit.IAM.Rsa;
 namespace YaCloudKit.IAM.Jwt;
 
 public class YandexJsonWebTokenGenerator(IOptionsMonitor<YandexIamOptions> options, IYandexRsaFactory rsaFactory)
+	: IDisposable
 {
-    public async Task<string> GenerateJwtAsync(CancellationToken cancellationToken = default)
-    {
-        var optionsValue = options.CurrentValue;
-        ArgumentNullException.ThrowIfNull(optionsValue.ServiceAccountId);
-        ArgumentNullException.ThrowIfNull(optionsValue.PublicKeyId);
+	private bool _disposed;
+	private RSA? _rsa;
 
-        using var rsa = await rsaFactory.CreateRsaAsync(cancellationToken);
-        var header = new JwtHeader(new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSsaPssSha256))
-        {
-            { "kid", optionsValue.PublicKeyId }
-        };
+	public async Task<string> GenerateJwtAsync(CancellationToken cancellationToken = default)
+	{
+		if (_disposed)
+			throw new ObjectDisposedException(nameof(YandexJsonWebTokenGenerator));
 
-        var now = DateTimeOffset.UtcNow;
-        var claims = new List<Claim>
-        {
-            new(JwtRegisteredClaimNames.Aud, "https://iam.api.cloud.yandex.net/iam/v1/tokens"),
-            new(JwtRegisteredClaimNames.Iss, optionsValue.ServiceAccountId),
-            new("iat", now.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer),
-            new("exp", now.AddSeconds(3600).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer)
-        };
-        var payload = new JwtPayload(claims);
-        var jwtSecurityToken = new JwtSecurityToken(header, payload);
-        var handler = new JwtSecurityTokenHandler();
-        return handler.WriteToken(jwtSecurityToken);
-    }
+		var optionsValue = options.CurrentValue;
+		ArgumentNullException.ThrowIfNull(optionsValue.ServiceAccountId);
+		ArgumentNullException.ThrowIfNull(optionsValue.PublicKeyId);
+
+		_rsa ??= await rsaFactory.CreateRsaAsync(cancellationToken);
+		var header = new JwtHeader(new SigningCredentials(new RsaSecurityKey(_rsa), SecurityAlgorithms.RsaSsaPssSha256))
+		{
+			{ "kid", optionsValue.PublicKeyId }
+		};
+
+		var now = DateTimeOffset.UtcNow;
+		var claims = new List<Claim>
+		{
+			new(JwtRegisteredClaimNames.Aud, "https://iam.api.cloud.yandex.net/iam/v1/tokens"),
+			new(JwtRegisteredClaimNames.Iss, optionsValue.ServiceAccountId),
+			new("iat", now.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer),
+			new("exp", now.AddSeconds(3600).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer)
+		};
+		var payload = new JwtPayload(claims);
+		var jwtSecurityToken = new JwtSecurityToken(header, payload);
+		var handler = new JwtSecurityTokenHandler();
+		return handler.WriteToken(jwtSecurityToken);
+	}
+
+	public void Dispose()
+	{
+		if (_disposed)
+			return;
+		_rsa?.Dispose();
+		_disposed = true;
+	}
 }
